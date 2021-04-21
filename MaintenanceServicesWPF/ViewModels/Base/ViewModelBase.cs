f0cc01d5d978f4fe2672e308c692bd8f52c4b751
VDemyanov <vlad.demyanov.official@mail.ru>
2021-04-21 07:34:48 +0300

    WPF project: command, viewmodel

---
+++ MaintenanceServicesWPF/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace VDemyanov.MaintenanceServices.MaintenanceServicesWPF.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            // [CallerMemberName] позволяет вызывать метод не указывая в параметрах имя свойства
+            // компилятор сам подставит имя метода из которого вызывается OnPropertyChanged
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        /// <summary>
+        /// Разрешает кольцевые обновления свойств
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="PropertyName"></param>
+        /// <returns></returns>
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
